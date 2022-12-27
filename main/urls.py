8d57c45a13c546d10388e4d97c1e4170745513f6
AYurau <alex.iurov@yandex.ru>
2022-12-27 13:54:23 +0300

    added detail pages

--- main/urls.py
+++ main/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('rent.html', views.rent),
     path('bv115.html', views.bv115),
     path('cart.html', views.cart),
-    path('offer.html', views.offer),
+    path('offer_<int:card_id>.html', views.offer, name='card'),
     path('options.html', views.options),
     path('spare.html', views.spare),
+    path('refurbished_chassis.html', views.ref),
+    path('fullref.html', views.fullref)
 ]
--- main/views.py
+++ main/views.py
@@ -8,6 +8,38 @@ def index(request):
     return render(request, 'main/home.html')
 
 
+def fullref(request):
+    machine_options = {}
+    used_machines_models = ForSale.objects.all()
+    for machines in used_machines_models:
+        machin_options_str = machines.options
+        machin_options_lst = []
+        for i in machin_options_str.split(','):
+            opt = i.strip().capitalize()
+            machin_options_lst.append(opt)
+        else:
+            machine_options[machines.title] = machin_options_lst
+    else:
+        return render(request, 'main/fullref.html', {'used_machines_models': used_machines_models,
+                                                     'machine_options': machine_options})
+
+
+def ref(request):
+    machine_options = {}
+    used_machines_models = ForSale.objects.all()
+    for machines in used_machines_models:
+        machin_options_str = machines.options
+        machin_options_lst = []
+        for i in machin_options_str.split(','):
+            opt = i.strip().capitalize()
+            machin_options_lst.append(opt)
+        else:
+            machine_options[machines.title] = machin_options_lst
+    else:
+        return render(request, 'main/refurbished_chassis.html', {'used_machines_models': used_machines_models,
+                                                                 'machine_options': machine_options})
+
+
 def used_machines(request):
     machine_options = {}
     used_machines_models = ForSale.objects.all()
@@ -32,8 +64,18 @@ def cart(request):
     return render(request, 'main/cart.html')
 
 
-def offer(request):
-    return render(request, 'main/offer.html')
+def offer(request, card_id):
+    obj = ForSale.objects.get(pk=card_id)
+    machine_options = {}
+    machin_options_str = obj.options
+    machin_options_lst = []
+    for i in machin_options_str.split(','):
+        opt = i.strip().capitalize()
+        machin_options_lst.append(opt)
+    else:
+        machine_options[obj.title] = machin_options_lst
+    return render(request, 'main/offer.html',{'obj':obj,
+                                              'machine_options':machine_options})
 
 
 def options(request):
